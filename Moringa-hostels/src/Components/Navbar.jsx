8e1b42292fa632607d73b585e439d4afeb818c61
sharonodongo <sharon94odongo@gmail.com>
2025-02-25 13:34:04 +0300

    adjusted navbar

--- Moringa-hostels/src/Components/Navbar.jsx
+++ Moringa-hostels/src/Components/Navbar.jsx
@@ -1,24 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "/src/index.css";
+import "/src/index.css"; 
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <div className="logo">
-        <img src="/logo.png" alt="Moringa Hostels" />
-      </div>
+      <div>
+        {/* Logo */}
+        <div className="logo">
+          <img src="/logo.png" alt="Moringa Hostels" />
+        </div>
 
-      <ul className="nav-links">
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/accommodations">Accommodations</Link></li>
-        <li><Link to="/contact">Contact Us</Link></li>
-      </ul>
+        {/* Navigation Links */}
+        <ul className="nav-links">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/accommodations">Accommodations</Link></li>
+          <li><Link to="/contact">Contact Us</Link></li>
+        </ul>
 
-      <div className="nav-buttons">
-        <button className="login-btn">Log In</button>
-        <button className="signup-btn">Sign Up</button>
+        {/* Buttons */}
+        <div className="nav-buttons">
+          <button className="login-btn">Log In</button>
+          <button className="signup-btn">Sign Up</button>
+        </div>
       </div>
     </nav>
   );
