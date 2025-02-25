b6f2ad3b3f4d7796c92a48faec4304b8685bc4e7
sharonodongo <sharon94odongo@gmail.com>
2025-02-25 12:44:19 +0300

    initial commit

--- Moringa-hostels/src/App.jsx
+++ Moringa-hostels/src/App.jsx
@@ -1,18 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./Components/Navbar";
+import Navbar from "./Components/Header";
 import Home from "./Components/Home";
-import Accommodations  from "./Components/Accommodations ";
-import Dashboard from "./Components/Dashboard"
+import About from "./Components/About";
+import Accommodations from "./Components/Accommodations"; 
+import ContactUs from "./Components/ContactUs"; 
+
 
 function App() {
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />} /> 
+        <Route path="/about" element={<About />} />
         <Route path="/accommodations" element={<Accommodations />} />
-        
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/contactus" element={<ContactUs />} /> 
       </Routes>
     </>
   );
