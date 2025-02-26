d1364cfce6b1a331a7af66801143ffb78bfd5147
sharonodongo <sharon94odongo@gmail.com>
2025-02-26 21:42:20 +0300

    adding accommodation page

--- Moringa-hostels/src/App.jsx
+++ Moringa-hostels/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home"; 
 import About from "./components/About"
+import Accommodations from "./components/Accommodations";
 import "./index.css";
 
 function App() {
@@ -12,6 +13,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/accommodations" element={<Accommodations />} />
+
       </Routes>
     </Router>
   );
